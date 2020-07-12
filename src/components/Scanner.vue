0e3ebf6444351a587e9f903a355c9551c139b7b2
Aries Cs <aries0d0f@gmail.com>
2020-07-12 17:48:44 +0800

    feat(view): Bind methods into `Scanner` component.

--- src/components/Scanner.vue
+++ src/components/Scanner.vue
@@ -8,11 +8,19 @@
 
 <script lang="ts">
 import { Component, Vue } from "vue-property-decorator";
+import { Action } from "vuex-class";
+
+import { FormState } from "@/store/types/form.type";
 
 @Component
 export default class Scanner extends Vue {
+  @Action("setToken", { namespace: "form" })
+  private setToken!: (token: FormState["token"]) => void;
+
   private onDecode(decoded: string): void {
-    console.log(decoded);
+    if (decoded && typeof decoded === "string") {
+      this.setToken(decoded);
+    }
   }
 }
 </script>
