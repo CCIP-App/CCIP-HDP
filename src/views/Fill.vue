abb809f7561e525fa4f0ac9a4a2b9e494eeeccba
Aries Cs <aries0d0f@gmail.com>
2020-07-12 19:41:54 +0800

    feat(view): Add Fill basic component.

--- src/views/Fill.vue
+++ src/views/Fill.vue
@@ -1,10 +1,37 @@
 <template>
-  <div class="fill-container" />
+  <div class="fill-container">
+    <div v-for="field in fields" :key="field.type" class="fill">
+      <label :for="field.name" class="fill fill-label">
+        <span>{{ field.label.zh }}｜{{ field.label.en }}</span>
+      </label>
+      <input
+        :type="field.type"
+        :name="field.name"
+        v-model="fieldData[field.name]"
+        class="fill fill-inputbox"
+        required
+      />
+    </div>
+  </div>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from "vue-property-decorator";
+import { Getter } from "vuex-class";
+
+import { FormState } from "@/store/types/form.type";
 
 @Component
-export default class Fill extends Vue {}
+export default class Fill extends Vue {
+  @Getter("field", { namespace: "form" })
+  private fields!: FormState["field"];
+
+  private fieldData: { [key: string]: string } = {};
+
+  private mounted(): void {
+    this.fields.forEach(field => {
+      this.fieldData[field.name] = "";
+    });
+  }
+}
 </script>
