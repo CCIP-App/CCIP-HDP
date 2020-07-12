47b4485b6d6d6fd4168cf7617150c0890002c692
Aries Cs <aries0d0f@gmail.com>
2020-07-12 19:04:50 +0800

    feat(view): `Navbar` component and stylesheets.

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,14 +1,15 @@
 <template>
-  <nav class="nav">
-    <ul>
+  <nav class="navbar navbar-container">
+    <ul class="navbar">
       <li
-        v-for="step in routeList
+        v-for="(step, index) in routeList
           .filter(route => route.meta.type === 'Step')
           .sort((a, b) => (a.meta.index > b.meta.index ? 1 : -1))"
         :key="step.name"
-        :class="{ active: $route.meta.index <= step.meta.index }"
-        class="step"
+        :class="{ active: $route.meta.index >= step.meta.index }"
+        class="navbar navbar-step-item"
       >
+        <span class="font-code">{{ index + 1 }}</span>
         <span>{{ step.meta.label.zh }}</span>
         <span>{{ step.meta.label.en }}</span>
       </li>
