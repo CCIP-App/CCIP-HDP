e6d73ca02276bfeaaef8019d54a962f64dd4b1d4
Aries Cs <aries0d0f@gmail.com>
2020-07-12 17:47:56 +0800

    feat(view): Bind methods indo `Landing` component.

--- src/views/Landing.vue
+++ src/views/Landing.vue
@@ -1,10 +1,60 @@
 <template>
-  <div class="landing-container" />
+  <div class="landing-container">
+    <Scanner />
+    <div
+      v-if="typeof status !== null"
+      :class="{ passed: status }"
+      class="status-icon"
+    >
+      <font-awesome-icon v-if="status" :icon="['fas', 'check']" />
+      <font-awesome-icon v-else :icon="['fas', 'times']" />
+    </div>
+  </div>
 </template>
 
 <script lang="ts">
-import { Component, Vue } from "vue-property-decorator";
+import { Component, Watch, Vue } from "vue-property-decorator";
+import { Action, Getter } from "vuex-class";
 
-@Component
-export default class Landing extends Vue {}
+import { AppState } from "@/store/types/app.type";
+import { FormState, FormResponse } from "@/store/types/form.type";
+
+import Scanner from "@/components/Scanner.vue";
+
+@Component({
+  components: {
+    Scanner
+  }
+})
+export default class Landing extends Vue {
+  @Action("verifyToken", { namespace: "form" })
+  private verifyToken!: (args: {
+    endpoint: AppState["endpoint"];
+    token: FormState["token"];
+  }) => Promise<FormResponse>;
+
+  @Getter("endpoint", { namespace: "app" })
+  private endpoint!: string;
+
+  @Getter("token", { namespace: "form" })
+  private token!: string;
+
+  @Watch("token")
+  private async onTokenChange(token: string) {
+    const res = (await this.verifyToken({
+      endpoint: this.endpoint,
+      token: token
+    })) as { fill: string; status: string };
+
+    if (!res.fill) {
+      this.$router.push({ name: "Fill" });
+    } else if (res.status) {
+      this.status = true;
+    } else {
+      this.status = false;
+    }
+  }
+
+  private status: boolean | null = null;
+}
 </script>
