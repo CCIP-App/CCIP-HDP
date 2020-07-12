2291474ca3df6d0603d300ea54a44ee25a488f8a
Aries Cs <aries0d0f@gmail.com>
2020-07-12 16:09:38 +0800

    feat(store): Bind module to Vuex.Store.

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,11 +1,14 @@
 import Vue from "vue";
 import Vuex from "vuex";
 
+import { app } from "@/store/modules/app";
+import { declaration } from "@/store/modules/declaration";
+
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-  state: {},
-  mutations: {},
-  actions: {},
-  modules: {}
+  modules: {
+    app,
+    declaration
+  }
 });
