9ccb6db16a39c8d71b342a57653dfa5b638a3662
Aries Cs <aries0d0f@gmail.com>
2020-07-12 14:46:36 +0800

    feat(view): Add basic components.

---
+++ src/components/Navbar.vue
@@ -0,0 +1,27 @@
+<template>
+  <nav class="nav">
+    <ul>
+      <li
+        v-for="step in routeList
+          .filter(route => route.meta.type === 'Step')
+          .sort(route => route.meta.index)"
+        :key="step.name"
+        :class="{ active: $route.meta.index <= step.meta.index }"
+        class="step"
+      >
+        <span>{{ step.meta.label.zh }}</span>
+        <span>{{ step.meta.label.en }}</span>
+      </li>
+    </ul>
+  </nav>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from "vue-property-decorator";
+import { routes } from "@/router";
+
+@Component
+export default class Navbar extends Vue {
+  private routeList = routes;
+}
+</script>
--- src/components/Scanner.vue
+++ src/components/Scanner.vue
@@ -1,14 +1,18 @@
 <template>
   <div class="scanner-container">
-    <div class="scanner"></div>
+    <div class="scanner">
+      <qrcode-stream @decode="onDecode" />
+    </div>
   </div>
 </template>
 
 <script lang="ts">
-import { Component, Prop, Vue } from "vue-property-decorator";
+import { Component, Vue } from "vue-property-decorator";
 
 @Component
 export default class Scanner extends Vue {
-  @Prop() private msg!: string;
+  private onDecode(decoded: string): void {
+    console.log(decoded);
+  }
 }
 </script>
