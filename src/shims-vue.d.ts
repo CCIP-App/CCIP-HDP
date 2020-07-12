5ac097287ef35fe8e40074391b5021c6042ca29d
Aries Cs <aries0d0f@gmail.com>
2020-07-12 16:10:05 +0800

    feat(deploy) Declare modules.

--- src/shims-vue.d.ts
+++ src/shims-vue.d.ts
@@ -3,4 +3,11 @@ declare module "*.vue" {
   export default Vue;
 }
 
+declare module "*.md" {
+  const content: string;
+  export default content;
+}
+
 declare module "vue-qrcode-reader"
+
+declare module "markdown-yaml-metadata-parser";
